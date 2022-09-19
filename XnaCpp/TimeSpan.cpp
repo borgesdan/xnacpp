194f40f3233e82a0eba8613a059f847dd5017f30
borgesdan <borges_santos89@hotmail.com>
2022-09-19 16:16:11 -0300

    feat: add TimeSpan struct

---
+++ XnaCpp/TimeSpan.cpp
@@ -0,0 +1,184 @@
+#include "TimeSpan.hpp"
+
+using std::numeric_limits;
+
+namespace Xna {
+	const TimeSpan TimeSpan::Zero = TimeSpan();
+
+	//----- Constructors -----
+
+	TimeSpan::TimeSpan() {}
+
+	TimeSpan::TimeSpan(int64_t ticks) :
+		_ticks(ticks) {}
+
+	TimeSpan::TimeSpan(int32_t hours, int32_t minutes, int32_t seconds) :
+		_ticks(TimeToTicks(hours, minutes, seconds)) {}
+
+	TimeSpan::TimeSpan(int32_t days, int32_t hours, int32_t minutes, int32_t seconds) :
+		_ticks(DayToTicks(days, hours, minutes, seconds, 0)) {}
+
+	TimeSpan::TimeSpan(int32_t days, int32_t hours, int32_t minutes, int32_t seconds, int32_t milliseconds) :
+		_ticks(DayToTicks(days, hours, minutes, seconds, milliseconds)) {}
+
+	//----- Operators -----
+	TimeSpan TimeSpan::operator- (TimeSpan const& t) {
+		return TimeSpan(-t._ticks);
+	}
+
+	TimeSpan TimeSpan::operator+ (TimeSpan const& t) {
+		return t;
+	}
+
+	TimeSpan operator+ (TimeSpan const& t1, TimeSpan const& t2) {
+		return t1.Add(t2);
+	}
+	
+	TimeSpan operator- (TimeSpan const& t1, TimeSpan const& t2) {
+		return t1.Subtract(t2);
+	}
+
+	bool operator== (TimeSpan const& t1, TimeSpan const& t2) {
+		return t1._ticks == t2._ticks;
+	}
+	
+	bool operator!= (TimeSpan const& t1, TimeSpan const& t2) {
+		return t1._ticks != t2._ticks;
+	}
+
+	bool operator< (TimeSpan const& t1, TimeSpan const& t2) {
+		return t1._ticks < t2._ticks;
+	}
+
+	bool operator<= (TimeSpan const& t1, TimeSpan const& t2) {
+		return t1._ticks <= t2._ticks;
+	}
+
+	bool operator> (TimeSpan const& t1, TimeSpan const& t2) {
+		return t1._ticks > t2._ticks;
+	}
+
+	bool operator>= (TimeSpan const& t1, TimeSpan const& t2) {
+		return t1._ticks >= t2._ticks;
+	}
+
+	//---- Static Functions -----
+
+	int32_t TimeSpan::Compare(TimeSpan const& t1, TimeSpan const& t2) {
+		if (t1._ticks > t2._ticks) 
+			return 1;
+
+		if (t1._ticks < t2._ticks) 
+			return -1;
+		
+		return 0;
+	}
+
+	TimeSpan TimeSpan::FromDays(double value) {
+		return Interval(value, MillisPerDay);
+	}
+
+	TimeSpan TimeSpan::FromHours(double value) {
+		return Interval(value, MillisPerHour);
+	}
+
+	TimeSpan TimeSpan::Interval(double value, int32_t scale) {
+		double tmp = value * scale;
+		double millis = tmp + (value >= 0 ? 0.5 : -0.5);
+		
+		return TimeSpan(static_cast<int64_t>(millis) * TicksPerMillisecond);
+	}
+
+	TimeSpan TimeSpan::FromMilliseconds(double value) {
+		return Interval(value, 1);
+	}
+
+	TimeSpan TimeSpan::FromMinutes(double value) {
+		return Interval(value, MillisPerMinute);
+	}
+
+	TimeSpan TimeSpan::FromSeconds(double value) {
+		return Interval(value, MillisPerSecond);
+	}
+
+	TimeSpan TimeSpan::FromTicks(int64_t value) {
+		return TimeSpan(value);
+	}	
+
+	//----- Functions -----
+
+	int64_t TimeSpan::Ticks() const { return _ticks; }
+	
+	int32_t TimeSpan::Days() const { return static_cast<int32_t>(_ticks / TicksPerDay); }
+	
+	int32_t TimeSpan::Hours() const { return static_cast<int32_t>((_ticks / TicksPerHour) % 24);  }
+	
+	int32_t TimeSpan::Milliseconds() const { return static_cast<int32_t>((_ticks / TicksPerMillisecond) % 1000); }
+	
+	int32_t TimeSpan::Minutes() const { return static_cast<int32_t>((_ticks / TicksPerMinute) % 60); }
+	
+	int32_t TimeSpan::Seconds() const { return static_cast<int32_t>((_ticks / TicksPerSecond) % 60); }
+	
+	double TimeSpan::TotalDays() const { return static_cast<double>(_ticks) * DaysPerTick; }
+	
+	double TimeSpan::TotalHours() const { return static_cast<double>(_ticks) * HoursPerTick; }
+	
+	double TimeSpan::TotalMilliseconds() const {
+		double temp = static_cast<double>(_ticks) * MillisecondsPerTick;
+		
+		if (temp > MaxMilliSeconds)
+			return  static_cast<double>(MaxMilliSeconds);
+
+		if (temp < MinMilliSeconds)
+			return  static_cast<double>(MinMilliSeconds);
+
+		return temp;
+	}
+	
+	double TimeSpan::TotalMinutes() const { return static_cast<double>(_ticks) * MinutesPerTick; }
+	
+	double TimeSpan::TotalSeconds() const { return static_cast<double>(_ticks) * SecondsPerTick; }
+
+	TimeSpan TimeSpan::Add(TimeSpan const& ts) const {
+		int64_t result = _ticks + ts._ticks;
+
+		return TimeSpan(result);
+	}
+
+	TimeSpan TimeSpan::Duration() const {
+		return TimeSpan(_ticks >= 0 ? _ticks : -_ticks);
+	}
+
+	bool TimeSpan::Equals(TimeSpan other) const {
+		return _ticks == other._ticks;
+	}
+
+	TimeSpan TimeSpan::Negate() const {
+		return TimeSpan(-_ticks);
+	}
+
+	TimeSpan TimeSpan::Subtract(TimeSpan const& ts) const {
+		int64_t result = _ticks - ts._ticks;
+		return TimeSpan(result);
+	}
+
+	//----- Private -----
+
+	int64_t TimeSpan::TimeToTicks(int32_t const& hour, int32_t const& minute, int32_t const& second) {
+		int64_t totalSeconds =
+			static_cast<int64_t>(hour) * 3600 +
+			static_cast<int64_t>(minute) * 60 +
+			static_cast<int64_t>(second);		
+
+		return totalSeconds * TicksPerSecond;
+	}
+
+	int64_t TimeSpan::DayToTicks(int32_t days, int32_t hours, int32_t minutes, int32_t seconds, int32_t milliseconds) {
+		int64_t totalMilliSeconds =
+			(static_cast<int64_t>(days) * 3600 * 24 +
+				static_cast<int64_t>(hours) * 3600 +
+				static_cast<int64_t>(minutes) * 60 + seconds) * 1000 + milliseconds;		
+
+		return totalMilliSeconds * TicksPerMillisecond;
+	}
+}
---
+++ XnaCpp/TimeSpan.hpp
@@ -0,0 +1,85 @@
+#ifndef _TIMESPAN_HPP_
+#define _TIMESPAN_HPP_
+
+#include <stdint.h>
+#include <limits>
+
+namespace Xna {
+	struct TimeSpan {
+	
+		static constexpr int64_t TicksPerMillisecond = 10000;
+		static constexpr int64_t TicksPerSecond = TicksPerMillisecond * 1000;
+		static constexpr int64_t TicksPerMinute = TicksPerSecond * 60;
+		static constexpr int64_t TicksPerHour = TicksPerMinute * 60;
+		static constexpr int64_t TicksPerDay = TicksPerHour * 24;
+
+		static const TimeSpan Zero;
+
+		TimeSpan();
+		TimeSpan(int64_t ticks);
+		TimeSpan(int32_t hours, int32_t minutes, int32_t seconds);
+		TimeSpan(int32_t days, int32_t hours, int32_t minutes, int32_t seconds);
+		TimeSpan(int32_t days, int32_t hours, int32_t minutes, int32_t seconds, int32_t milliseconds);
+
+		TimeSpan operator -(TimeSpan const& t);
+		TimeSpan operator +(TimeSpan const& t);
+		friend TimeSpan operator +(TimeSpan const& t1, TimeSpan const& t2);
+		friend TimeSpan operator -(TimeSpan const& t1, TimeSpan const& t2);
+		friend bool operator ==(TimeSpan const& t1, TimeSpan const& t2);
+		friend bool operator !=(TimeSpan const& t1, TimeSpan const& t2);
+		friend bool operator <(TimeSpan const& t1, TimeSpan const& t2);
+		friend bool operator <=(TimeSpan const& t1, TimeSpan const& t2);
+		friend bool operator >(TimeSpan const& t1, TimeSpan const& t2);
+		friend bool operator >=(TimeSpan const& t1, TimeSpan const& t2);
+
+		static int32_t Compare(TimeSpan const& t1, TimeSpan const& t2);
+		static TimeSpan FromDays(double value);
+		static TimeSpan FromHours(double value);
+		static TimeSpan Interval(double value, int32_t scale);
+		static TimeSpan FromMilliseconds(double value);
+		static TimeSpan FromMinutes(double value);
+		static TimeSpan FromSeconds(double value);
+		static TimeSpan FromTicks(int64_t value);
+
+		int64_t Ticks() const;
+		int32_t Days() const;
+		int32_t Hours() const;
+		int32_t Milliseconds() const;
+		int32_t Minutes() const;
+		int32_t Seconds() const;
+		double TotalDays() const;
+		double TotalHours() const;
+		double TotalMilliseconds() const;
+		double TotalMinutes() const;
+		double TotalSeconds() const;
+
+		TimeSpan Add(TimeSpan const& ts) const;
+		TimeSpan Duration() const;
+		bool Equals(TimeSpan other) const;
+		TimeSpan Negate() const;
+		TimeSpan Subtract(TimeSpan const& ts) const;
+
+	private:
+		static constexpr double MillisecondsPerTick = 1.0 / TicksPerMillisecond;
+		static constexpr double SecondsPerTick = 1.0 / TicksPerSecond;
+		static constexpr double MinutesPerTick = 1.0 / TicksPerMinute;
+		static constexpr double HoursPerTick = 1.0 / TicksPerHour;
+		static constexpr double DaysPerTick = 1.0 / TicksPerDay;
+		static constexpr int32_t MillisPerSecond = 1000;
+		static constexpr int32_t MillisPerMinute = MillisPerSecond * 60;
+		static constexpr int32_t MillisPerHour = MillisPerMinute * 60;
+		static constexpr int32_t MillisPerDay = MillisPerHour * 24;
+		static constexpr int64_t MaxSeconds = std::numeric_limits<int64_t>::max() / TicksPerSecond;
+		static constexpr int64_t MinSeconds = std::numeric_limits<int64_t>::min() / TicksPerSecond;
+		static constexpr int64_t MaxMilliSeconds = std::numeric_limits<int64_t>::max() / TicksPerMillisecond;
+		static constexpr int64_t MinMilliSeconds = std::numeric_limits<int64_t>::min() / TicksPerMillisecond;
+		static constexpr int64_t TicksPerTenthSecond = TicksPerMillisecond * 100;
+
+		int64_t _ticks{ 0 };
+
+		static int64_t TimeToTicks(int32_t const& hour, int32_t const& minute, int32_t const& second);
+		static int64_t DayToTicks(int32_t days, int32_t hours, int32_t minutes, int32_t seconds, int32_t milliseconds);
+	};
+}
+
+#endif
